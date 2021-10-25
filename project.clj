07b4ebe585d588b7650fab87c456c49b7a38607c
benjamin <benjamin@localhost.localdomain>
2021-10-25 11:36:32 -0400

    Change license to MIT

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject cow-face-wave "0.1.0-SNAPSHOT"
   :description "Print out cow faces in a sinusoidal fashion"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot cow-face-wave.core
   :target-path "target/%s"
