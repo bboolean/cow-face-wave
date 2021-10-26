4da611b8632d43e16b28202a189a56e2dad78e4f
benjamin <benjamin@localhost.localdomain>
2021-10-26 18:43:26 -0400

    Add period to description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cow-face-wave "0.1.0-SNAPSHOT"
-  :description "Print out cow faces in a sinusoidal fashion"
+  :description "Print out cow faces in a sinusoidal fashion."
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot cow-face-wave.core
   :target-path "target/%s"
